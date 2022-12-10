1d5cffb551bcb58ece3d100d12d8124da57fcb04
Jossoft <ydilekci1@gmail.com>
2022-12-10 15:08:17 +0300

    Add project files.

---
+++ BlogApp/Controllers/BlogController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult WriteBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult WriteBlog(BlogCategoryUser blogCategoryUser)
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+
+        public IActionResult GetBlogById()
+        {
+            return View();
+        }
+
+        public IActionResult GetBlogByCategory()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BlogApp/Models/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogApp.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set;}
+        public string BlogContent { get; set;}
+    }
+}
